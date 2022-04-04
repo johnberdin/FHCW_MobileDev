9c134e7ccbdcfd57bb3f566145c6a7e89dec482a
unknown <john.berdin@stud.fh-campuswien.ac.at>
2022-04-04 23:03:06 +0200

    first

---
+++ app/build/generated/source/buildConfig/debug/com/example/movieapplication/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.movieapplication;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.movieapplication";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
